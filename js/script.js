7970f92b656a14bd86e01a2930527bea6b1edb01
TwoDom <ytworek@gmail.com>
2017-07-07 15:44:51 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/*Set actual year*/
+var thisDate = new Date();
+var thisYear = thisDate.getFullYear();
+document.getElementById("initYear").value = thisYear;
+
+/*Change year*/
+function modifyYear(val){
+  var actualYear = document.getElementById("initYear").value;
+  var newYear = parseInt(actualYear,10) + val;
+
+  document.getElementById("initYear").value = newYear;
+  return newYear;
+}
+
+/*Show month details*/
+function myFunction(event) {
+    var month = event.target;
+    var activeMonth = parseInt(month.id);
+    var activeYear = parseInt(document.getElementById("initYear").value);
+
+    document.getElementById(month.id).classList.add("active");
+    // document.querySelector("ul").classList.remove("hide");
+
+    function getDaysInMonth(month, year) {
+      var calendar = document.createElement("span");
+      // document.getElementById(month.id).appendChild(calendar);
+        var date = new Date(year, month, 1);
+        var days = [];
+        while (date.getMonth() === month) {
+            days.push(new Date(date));
+
+            date.setDate(date.getDate() + 1);
+        }
+      console.log(days);
+      return days;
+    }
+
+    getDaysInMonth(activeMonth, activeYear);
+}
